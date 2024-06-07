6dc842060134c75dcf79cd6378aa4bd354a4b564
SMA <SMA@d.c>
2024-06-07 18:06:37 +0300

    Added safe

---
+++ Assets/project/Scripts/MVP/Views/Safe.cs
@@ -0,0 +1,28 @@
+﻿using Assets.project.Scripts.UIComponents;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.project.Scripts.MVP.Views
+{
+    public class Safe : MonoBehaviour
+    {
+        [SerializeField] private List<Number> _numbers;
+
+        private int _code = 3689;
+
+        public void Open()
+        {
+            string userCode = "";
+
+            foreach (var number in _numbers)
+            {
+                userCode += number.NumberValue.ToString();
+            }
+
+            if(int.Parse(userCode) == _code)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
---
+++ Assets/project/Scripts/UIComponents/Number.cs
@@ -0,0 +1,33 @@
+﻿using Assets.project.Scripts.Common;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.project.Scripts.UIComponents
+{
+    public class Number : MonoBehaviour
+    {
+        [SerializeField] private Image _imageNumber;
+
+        private int _number = 0;
+
+        public int NumberValue { get => _number; }
+
+        public void Up()
+        {
+            _number += 1;
+            if (_number > 9)
+                _number = 0;
+
+            ImageLoader.LoadUIImage(_number.ToString());
+        }
+
+        public void Down() 
+        {
+            _number -= 1;
+            if (_number < 0)
+                _number = 9;
+
+            ImageLoader.LoadUIImage(_number.ToString());
+        }
+    }
+}
