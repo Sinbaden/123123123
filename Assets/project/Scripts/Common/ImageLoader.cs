0e166d2f054d493e9125b2e4b6142ef08e08e601
ArhangelM <73641545+ArhangelM@users.noreply.github.com>
2024-06-07 18:08:48 +0300

    Added safe (#3)
    Co-authored-by: SMA <SMA@d.c>

---
+++ Assets/project/Scripts/Common/ImageLoader.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Assets.project.Scripts.Common
+{
+    public class ImageLoader
+    {
+        private static string uiPath = "Images/UI/";
+
+        public static Sprite LoadUIImage(string path) => Resources.Load<Sprite>(uiPath + path);
+    }
+}
